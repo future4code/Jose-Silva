623cd4894119ce312f38b15e16414051ae3f8c07
Jose <jose.renato@al.infnet.edu.br>
2021-09-07 12:02:34 -0300

    exercicios-variaveis-JS

---
+++ quinzena1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,59 @@
+/*1)
+10
+10,5 
+
+2) 10,10,10
+
+3) let horasDia = prompt("Quantas horas você trabalha por dia?")
+let valorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasDias/valorDia} por hora`)
+
+
+*/
+
+// #Exercises code
+/*  1)
+
+let nome;
+let idade;
+
+console.log(typeof nome,typeof idade);
+// como não foi definido nenhum valor as variaveis, o resultado é: Indefinido
+
+nome = prompt("Qual o seu Nome: ");
+
+idade = prompt("Qual sua idade?");
+console.log(typeof nome,typeof idade);
+// notei que imprime 2 strings, mesmo digitando numero na idade
+console.log(`Olá ${nome},  você tem ${idade} anos`); */
+
+//2) a) b)
+/* homem = prompt("Você É Homem?");
+console.log("Você é homem?",homem);
+var mulher = prompt("Você É Mulher?");
+console.log("Você É Mulher?",mulher);
+var casado = prompt("Você É Casado?");
+console.log("Você É Casado(a)?",casado);
+
+//3 
+let a = 12;
+let b = 21;
+let c = a;
+a = b;
+b = c;
+
+console.log(a, b); */
+
+//Desafio
+let n1 = prompt("Digite o primeiro numero:");
+let n2 = prompt("Digite o segundo numero:");
+let soma = parseInt(n1) + parseInt(n2);
+let mult = parseInt(n1) * parseInt(n2);
+console.log(" O primeiro número somado ao segundo número resulta em: ",soma);
+console.log(" O primeiro número multiplicado pelo segundo número resulta em:", mult);
+
+
+
+
+
+

518432398c0cdbdbdd00ffdb4fb94d6420619b8b
Jose <jose.renato@al.infnet.edu.br>
2021-09-10 14:12:47 -0300

    execícios-operadores-js

---
+++ quinzena1/operadores/template_javascript/index.js
@@ -0,0 +1,79 @@
+/* 
+1)
+
+    a. false
+    b. false
+    c. true
+    d. boolean
+
+2) Esta faltando a conversão.
+Será impresso a concatenação
+Ex: n1 = 10; n2 = 2;
+resultado: 102;    
+
+3) 
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = parseInt(primeiroNumero) + parseInt(segundoNumero);
+
+console.log(soma)
+
+*/
+
+/*
+1)
+
+
+let idade = prompt("Qual sua idade?")
+let idadeAmigo = prompt("Qual é a idade do seu melhor amigo ou da sua melhor amiga?");
+
+let idadeMaior = idade > idadeAmigo ? true : false;
+console.log("Você é mais velho(A) que seu amigo?", idadeMaior);
+console.log("Diferença de idade:", (idade - idadeAmigo));
+*/
+/*
+2)
+
+
+
+let par = prompt("Digite um número par: ")
+console.log( par % 2);
+// Os numero inteiros pares ao serem dividos por 2 retornam resto 0
+// Os numero inteiros impares ao serem dividos por 2 retornam resto 1
+
+*/
+
+/*
+3) 
+
+
+let idade = prompt("Digite sua idade: ");
+
+let idadeMeses = idade * 12;
+let idadeDias = idade * 365;
+let idadeHoras = idadeDias * 24;
+
+console.log("Idade em Meses:", idadeMeses);
+console.log("Idade em Dias:", idadeDias);
+console.log("Idade em Horas:", idadeHoras);
+*/
+
+/*
+4)
+
+
+let primeiroNumero = prompt("Digite um número!")
+let segundoNumero = prompt("Digite outro número!");
+
+let maior = parseInt(primeiroNumero) > parseInt(segundoNumero) ? true:false;
+let igual = parseInt(primeiroNumero) === parseInt(segundoNumero) ? true:false;
+let divisivel2 = parseInt(primeiroNumero) % parseInt(segundoNumero) === 0 ? true:false;
+let divisivel1 = parseInt(segundoNumero) % parseInt(primeiroNumero) === 0 ? true:false;
+
+console.log("O primeiro numero é maior que segundo?",maior);
+console.log("O primeiro numero é igual ao segundo?",igual);
+console.log("O primeiro numero é divisível pelo segundo?",divisivel2);
+console.log("O segundo numero é divisível pelo primeiro? ",divisivel1);
+
+*/

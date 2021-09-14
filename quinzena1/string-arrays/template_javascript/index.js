264b3eb942a7fc4f03a1e801b638199a0182968c
Jose <jose.renato@al.infnet.edu.br>
2021-09-14 20:00:58 -0300

    execícios-arrays-string

---
+++ quinzena1/string-arrays/template_javascript/index.js
@@ -0,0 +1,55 @@
+/* a. undefined 
+  b. null
+c. 11
+d. 3
+e. array[1] = 19
+f. 9
+
+2) SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+*/
+
+
+/* const nomeUsuario = prompt("Digite seu nome de Usuário:");
+const emailUsuario = prompt("Digite seu email:");
+
+console.log(`O e-mail ${emailUsuario} foi cadastrado com sucesso. 
+Seja bem-vinda(o), ${nomeUsuario}!
+`) */
+
+/* let comidasPrediletas = ["Camarão", "Peixe", "Lasanha", "Strogonoff", "Picanha"];
+console.log(comidasPrediletas);
+
+console.log(`Essas são minhas comidas preferidas:`);
+console.log(comidasPrediletas[0]);
+console.log(comidasPrediletas[1]);
+console.log(comidasPrediletas[2]);
+console.log(comidasPrediletas[3]);
+console.log(comidasPrediletas[4]);
+
+const comidaUsuario = prompt("Digite sua comida predileta: ");
+
+comidasPrediletas[1] = comidaUsuario;
+console.log(comidasPrediletas);
+ */
+
+/* 
+let listaDeTarefas = [];
+const tarefa1 = prompt("Digite 3 tarefas: ");
+const tarefa2 = prompt("Digite a segunda tarefa: ");
+const tarefa3 = prompt("Digite a terceira: ");
+
+listaDeTarefas.push(tarefa1);
+listaDeTarefas.push(tarefa2);
+listaDeTarefas.push(tarefa3);
+console.log(listaDeTarefas);
+
+const indice = prompt("Digite o indice de uma dessas tarefas: 0, 1 ou 2");
+
+listaDeTarefas.splice(indice, 1);
+console.log(listaDeTarefas); */
+
+let frase = prompt("Digite uma frase: ");
+  let fraseArray = [];
+  fraseArray.push(frase.split(" "));
+console.log(fraseArray);

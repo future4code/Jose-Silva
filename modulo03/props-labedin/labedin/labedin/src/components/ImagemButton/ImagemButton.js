196176ed05a1961cddd2bf4de18e82c232b690f6
Jose <jose.renato@al.infnet.edu.br>
2021-11-15 10:56:55 -0300

    curriculo-react-styledC

---
+++ modulo03/props-labedin/labedin/labedin/src/components/ImagemButton/ImagemButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+const ImagemDivBtn = styled.div `
+display: flex;
+align-items: center;
+border: 1px solid black;
+border-radius: 50px;
+width: 200px;
+padding: 15px 30px;
+margin: 10px auto;
+`
+
+const ImagemImg = styled.img `
+width: 30px;
+margin-right: 10px;
+` 
+
+function ImagemButton(props) {
+    return (
+
+        <>
+        <ImagemDivBtn>
+            <ImagemImg src={ props.imagem }/>
+            <p>{ props.texto }</p>
+        </ImagemDivBtn>
+
+        </>
+
+    )
+}
+
+export default ImagemButton;

de30154168f81ac13c3df158315b51f4092c687e
Jose <jose.renato@al.infnet.edu.br>
2021-11-19 21:47:36 -0300

    front-whats

---
+++ modulo03/projeto-whatslab/msg-whatsapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <footer>
+          <input type="text" id="user" placeholder="Usúario"/>
+          <input type="text" id="msg" placeholder="Mensagem"/>
+          <button  id="btn" type="button">Enviar</button>
+          </footer>
+    </div>
+  );
+}
+
+export default App;
